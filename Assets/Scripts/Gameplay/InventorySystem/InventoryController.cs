d271e650e6c889145ae03325ff859acaf0f95ca9
John Miragias <ioannis8@hotmail.com>
2022-01-22 14:14:59 +0200

    Small commit

--- Assets/Scripts/Gameplay/InventorySystem/InventoryController.cs
+++ Assets/Scripts/Gameplay/InventorySystem/InventoryController.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InventoryController : SingletonMono<InventoryController>
 {
     [SerializeField] private InventoryItemType[] ItemsInInventory;
     [SerializeField] private InventoryItemType m_LastItemClickedOnUI;
+    [SerializeField] private BoolVariable m_IsShowingDialogue;
 
     private void Update()
     {
+        if (m_IsShowingDialogue.Value) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+        }
     }
 }
