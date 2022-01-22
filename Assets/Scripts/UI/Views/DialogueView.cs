b1b610c8b9664634905d00eb37aa9987a503ad7a
John Miragias <ioannis8@hotmail.com>
2022-01-22 10:43:26 +0200

    Work on dialogues etc

---
+++ Assets/Scripts/UI/Views/DialogueView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class DialogueView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI m_DialogueText;
+
+    public void PlayDialogueText(string text)
+    {
+        m_DialogueText.text = string.Empty;
+    }
+
+    private IEnumerator TypeMachineText(string text)
+    {
+        int numberOfLetters = text.Length;
+        yield return null;
+    }
+}
