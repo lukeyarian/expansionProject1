96cfd05976cd88c5528a326f351626fef62eaab7
John Miragias <ioannis8@hotmail.com>
2022-01-23 10:41:55 +0200

    Make clock invisible on one world

--- Assets/Scripts/Gameplay/WorldChangeController.cs
+++ Assets/Scripts/Gameplay/WorldChangeController.cs
@@ -8,6 +8,8 @@ public class WorldChangeController : SingletonMono<WorldChangeController>
     [SerializeField] private SpriteRenderer m_Door;
     [SerializeField] private BoxCollider2D m_ClockCollider;
     [SerializeField] private Color m_FalseWordlColor;
+    
+    [SerializeField] private SpriteRenderer[] m_StuffThatExistInRealWorldOnly;
 
     private void Update()
     {
@@ -22,5 +24,14 @@ public class WorldChangeController : SingletonMono<WorldChangeController>
     {
         m_ClockCollider.enabled = realWorld;
         m_Background.color = realWorld ? Color.white : m_FalseWordlColor;
+        SetRenderersDependingOnWorld(realWorld);
+    }
+
+    private void SetRenderersDependingOnWorld(bool realWorld)
+    {
+        for (int i = 0; i < m_StuffThatExistInRealWorldOnly.Length; i++)
+        {
+            m_StuffThatExistInRealWorldOnly[i].enabled = realWorld;
+        }
     }
 }
