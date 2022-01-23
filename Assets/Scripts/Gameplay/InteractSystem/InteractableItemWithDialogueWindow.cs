9f8a0a52bd6d873134d700bf9bb1b1d8752a6dd2
John Miragias <ioannis8@hotmail.com>
2022-01-23 03:43:04 +0200

    Interactale with dialogue

---
+++ Assets/Scripts/Gameplay/InteractSystem/InteractableItemWithDialogueWindow.cs
@@ -0,0 +1,27 @@
+﻿public class InteractableItemWithDialogueWindow : InteractableItemWithDialogue 
+{
+    public override void Interact()
+    {
+        if (!EventConditionBooleans.HasFinishedInteractionWithPlant) return;
+        StartDialogue(WorldChangeController.Instance.IsNormalWorld? m_DefaultDialogue : m_FantasyDialogue , true);
+    }
+    
+    public override void InteractWithItem(InventoryItemType incomingItemType)
+    {
+        if (!EventConditionBooleans.HasInteractedWithWindow || !WorldChangeController.Instance.IsNormalWorld) return;
+        if (incomingItemType == InventoryItemType.PIZZA)
+        {
+            //StartDialogue(m_SpiderDialogue , false);
+        }
+        if (incomingItemType == InventoryItemType.SPIDER)
+        {
+            StartDialogue(m_SpecialDialoguesForInteractSuccessful , false);
+        }
+    }
+    
+    protected override void FinishDialogue(bool isDefault)
+    {
+        base.FinishDialogue(isDefault);
+        EventConditionBooleans.HasInteractedWithWindow = true;
+    }
+}
